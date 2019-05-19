5339582044d7cb2b97fdecfb067b8ca1dc0c6972
hefang <he@hefang.link>
2019-05-19 09:39:32 +0800

    修改.d.ts文件

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
 import * as React from "react";
 
-declare module "react-catchable" {
-    export type ErrorChild = React.ReactNode | ((error: Error, errorInfo: React.ErrorInfo) => React.ReactNode);
-    export default function catchable<T>(WrappedComponent: React.ComponentType<T>, errorChild?: ErrorChild): React.ComponentType<T>;
-}
+export type ErrorChild = React.ReactNode | ((error: Error, errorInfo: React.ErrorInfo) => React.ReactNode);
+export default function catchable<T>(WrappedComponent: React.ComponentType<T>, errorChild?: ErrorChild): React.ComponentType<T>;
